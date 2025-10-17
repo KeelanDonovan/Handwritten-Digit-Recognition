25f6d3d2f38f34008008554c5452488dec5da74d
Keelan Donovan <keelankld@gmail.com>
2025-10-16 23:22:06 -0400

    update model code

--- app/digit_recog_cnn.py
+++ app/digit_recog_cnn.py
@@ -2,23 +2,27 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-class DigitRecogCNN(nn.Module):
+class DigitRecognitionCNN(nn.Module):
   def __init__(self):
-    super(DigitRecogCNN, self).__init__()
+    super(DigitRecognitionCNN, self).__init__()
     self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, stride=1, padding=1)
+    self.dropout1 = nn.Dropout2d(0.25)
     self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=1)
-    self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
-    self.fully_connected1 = nn.Linear(32 * 3 * 3, 10)
+    self.dropout2 = nn.Dropout2d(0.25)
     self.flatten = nn.Flatten()
-  
+    self.fully_connected1 = nn.Linear(16 * 7 * 7, 128)
+    self.dropout3 = nn.Dropout(0.5)
+    self.fully_connected2 = nn.Linear(128, 10)
+
   def forward(self, x):
     x = F.relu(self.conv1(x))
     x = F.max_pool2d(x, 2)
+    x = self.dropout1(x)
     x = F.relu(self.conv2(x))
     x = F.max_pool2d(x, 2)
-    x = F.relu(self.conv3(x))
-    x = F.max_pool2d(x, 2)
+    x = self.dropout2(x)
     x = self.flatten(x)
-    x = self.fully_connected1(x)
+    x = F.relu(self.fully_connected1(x))
+    x = self.dropout3(x)
+    x = self.fully_connected2(x)
     return F.log_softmax(x, dim=1)
-    
